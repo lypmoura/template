fe6106b7a73561aef014724960ce0eb584de03dc
Felype José Moura De Santana <lypekjkj@icloud.com>
2025-12-06 23:24:23 -0300

    first commit

--- actions/get-dashboard-products.ts
+++ actions/get-dashboard-products.ts
@@ -5,7 +5,7 @@ import { d } from "@/services/drizzle";
 import { productsTable, usersTable } from "@/services/drizzle/schemas";
 import { eq } from "drizzle-orm";
 
-export const getDashboardProducts = async () => {
+export const getDashboardProducts = async (p0: string) => {
     const session = await auth();
 
     if (!session?.user?.id) {
